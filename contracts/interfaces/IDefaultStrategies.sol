680f26f4292c94c44acd0f5d1fb4cdfe7e611050
Conor Gallagher <dontlisten33@gmail.com>
2022-11-18 21:49:12 -0800

    IDefaultStrategies.sol

---
+++ contracts/interfaces/IDefaultStrategies.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract IDefaultStrategies {
+    address internal constant BALANCER_FLASHSWAP = 0x3E66B66Fd1d0b02fDa6C811Da9E0547970DB2f21;
+    address internal constant COMPOUND = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
+    address internal constant CURVE = 0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2;
+    address internal constant AAVE_V2 = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9;
+    address internal constant SUSHISWAP_ROUTER = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
+    address internal constant UNISWAP_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+}
